7a12f1c39ae75499b261f35b5fbe91cde34e2066
Pavel Plytaw <pavel.p@carouseller.com>
2025-08-28 12:05:32 +0200

    add generic customizable button

--- src/componenets/Button/index.js
+++ src/componenets/Button/index.js
@@ -1,12 +1,35 @@
 import "./index.css"
 
-const Button = ({ icon, text }) => {
-    return (
-        <div className="button">
-            <div className="button-icon">{icon}</div>
-            <p>{text}</p>
-        </div>
-    )
-}
+const Button = ({
+  variant = 'primary', // 'primary', 'secondary', 'dark', 'text', 'danger'
+  size = 'normal',
+  text,
+  icon,
+  className = '',
+  onClick,
+  disabled = false,
+  ...props
+}) => {
+  const buttonClasses = [
+    'default-btn',
+    `${variant}-button`,
+    size !== 'normal' ? `btn-${size}` : '',
+    className
+  ].filter(Boolean).join(' ');
+
+  return (
+    <button
+      className={buttonClasses}
+      onClick={onClick}
+      disabled={disabled}
+      {...props}
+    >
+      {icon && (
+        <div className="left-icon">{icon}</div>
+      )}
+      {text}
+    </button>
+  );
+};
 
 export default Button;
